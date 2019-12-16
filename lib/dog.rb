86eae60f512d008544d42feaa06b27e656c3443c
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-16 00:57:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+end
+.clear_all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(all dogs)
+    @@all = all dogs
+    @@all << all dogs
+  end
+ 
+  def clear.all
+    @@all
